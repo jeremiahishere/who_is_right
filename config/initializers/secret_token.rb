551656d5a3d98837b898cbb3b8ae295441ac9497
Jeremiah Hemphill <jeremiah@cloudspace.com>
2013-04-12 12:21:26 -0400

    Initial commi.t

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhoIsRight::Application.config.secret_token = '1f00193dd167bb34465f321686164e724b508fda1042a60edb7460db0978a09ecea44a212417eac88765f20b86e4650137a81da5690791bf632006d397e571fd'
